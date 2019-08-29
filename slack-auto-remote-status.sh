568e6893f5659d8b8414ae1b28dbd28fccbd29a7
Henrik Raitasola <henrik.raitasola@gmail.com>
2019-08-29 21:59:54 +0300

    Make office wifi configurable

--- slack-auto-remote-status.sh
+++ slack-auto-remote-status.sh
@@ -1,16 +1,21 @@
 
-TOKEN=$(</Users/henrikra/projects/slack-auto-remote-status/.slack-token.config)
-SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}')
+SLACK_TOKEN=$(</Users/henrikra/projects/slack-auto-remote-status/.slack-token.config)
 
-OFFICE_NETWORK="xxx"
+CURRENT_WIFI=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}')
+OFFICE_NETWORK=$(</Users/henrikra/projects/slack-auto-remote-status/.office-network.config)
 
-if [ $SSID == $OFFICE_NETWORK ]
+if [ $CURRENT_WIFI == $OFFICE_NETWORK ]
 then
-  STATUS='At%20Office'
+  STATUS='At Office'
 	EMOJI='office'
 else
-	STATUS='Working%20remotely'
+	STATUS='Working remotely'
 	EMOJI='house_with_garden'
 fi
 
-curl -X POST https://slack.com/api/users.profile.set\?profile\=%7B%0D%0A%20%20%20%20%22status_text%22%3A%20%22$STATUS%22%2C%0D%0A%20%20%20%20%22status_emoji%22%3A%20%22%3A$EMOJI%3A%22%0D%0A%7D\&token\=$TOKEN
+curl \
+	-X POST \
+	-H "Authorization: Bearer $SLACK_TOKEN" \
+	-H "Content-Type: application/json; charset=utf-8" \
+	-d '{"profile": {"status_text": "'"$STATUS"'", "status_emoji": "'":$EMOJI:"'"}}' \
+	https://slack.com/api/users.profile.set
